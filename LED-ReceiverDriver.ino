04b4356862e0581303c21837251d4821f00903d5
egezx <sunieetu@gmail.com>
2016-11-29 21:15:29 +0200

    Add files via upload

---
+++ LED-ReceiverDriver.ino
@@ -0,0 +1,153 @@
+
+#include <RH_NRF24.h>
+#include <SPI.h>
+
+#define CLIENT_ADDRESS 0x02
+#define SERVER_ADDRESS 0x01
+#define PAIR_ADDRESS 0x0A
+
+RH_NRF24 nrf24;
+
+
+
+
+//LED PARAMS
+bool ledPowerOn = false;
+
+const float ledMaxValue = 255.0;
+const float ledStepSize = 0.05;
+float ledBrightness = 1.0;
+float ledBrightnessDifference = 0.5;
+
+const byte ledPin1 = 3;
+const byte ledPin2 = 5;
+const byte pairPin = 4;
+
+bool pairButtonDown = false;
+unsigned long pairButtonTimer = millis();
+bool pairing = false;
+
+unsigned int pairTimeout = 2000;
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial); // wait for serial port to connect. Needed for Leonardo only
+  if (!nrf24.init())
+    Serial.println("init failed");
+  // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
+  if (!nrf24.setChannel(1))
+    Serial.println("setChannel failed");
+  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
+    Serial.println("setRF failed");
+
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(pairPin, INPUT_PULLUP);
+}
+
+
+void loop()
+{
+  PairButtonCheck();
+
+  if (pairing)
+  {
+    uint8_t data[2] = {PAIR_ADDRESS, SERVER_ADDRESS};
+    nrf24.send(data, sizeof(data));
+
+    delay(500);
+  }
+
+  else
+  {
+    if (nrf24.available())
+    {
+      uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+      uint8_t len = sizeof(buf);
+
+      if (nrf24.recv(buf, &len))
+      {
+        if (buf[0] == SERVER_ADDRESS)
+        {
+          Serial.println(buf[1], HEX);
+          switch (buf[1])
+          {
+            case 0xAA:
+              ledPowerOn = !ledPowerOn;
+              break;
+            case 0xBB:
+              UpdateLedParameter(&ledBrightnessDifference, ledStepSize);
+              break;
+            case 0xCC:
+              UpdateLedParameter(&ledBrightnessDifference, ledStepSize * (-1.0));
+              break;
+            case 0xDD:
+              UpdateLedParameter(&ledBrightness, ledStepSize);
+              break;
+            case 0xEE:
+              UpdateLedParameter(&ledBrightness, ledStepSize * (-1.0));
+              break;
+          }
+
+        }
+        /* Serial.print("Brightness Dirfference: ");
+         Serial.println(ledBrightnessDifference);
+         Serial.print("Brightness: ");
+         Serial.println(ledBrightness);*/
+
+        if (ledPowerOn)
+        {
+          analogWrite(ledPin1, (ledBrightnessDifference * ledBrightness * ledMaxValue));
+          analogWrite(ledPin2, ((1.0 - ledBrightnessDifference) * ledBrightness * ledMaxValue));
+        }
+        else
+        {
+          analogWrite(ledPin1, 0);
+          analogWrite(ledPin2, 0);
+        }
+      }
+    }
+  }
+}
+
+void PairButtonCheck()
+{
+  if (pairPin == LOW)
+  {
+    if (pairButtonDown)
+    {
+      if (millis() - pairButtonTimer > pairTimeout)
+        pairing = true;
+    }
+    else
+    {
+      pairButtonDown = true;
+      pairButtonTimer = millis();
+    }
+  }
+  else
+  {
+    pairing = false;
+    pairButtonDown = false;
+  }
+}
+
+
+void UpdateLedParameter( float* t_param, float t_val)
+{
+  float nextVal = *t_param + t_val;
+  if (nextVal >= -0.001 && nextVal <= 1.001)
+  {
+    /* Serial.print("Current Value (*param): ");
+     Serial.println(*t_param);
+     Serial.print("Value change: ");
+     Serial.println(t_val);
+
+     Serial.print("Next value: ");
+     Serial.println(fabs(nextVal));
+     */
+    *t_param = fabs(nextVal);
+  }
+}
+
---
+++ LED-Transmitter.ino
@@ -0,0 +1,291 @@
+
+#include <SPI.h>
+#include <RH_NRF24.h>
+
+#define CW  1
+#define CCW 0
+
+#define CLIENT_ADDRESS 0x02
+#define SERVER_ADDRESS 0x01
+
+
+//PinMapping
+const byte pinA = 3;
+const byte pinB = 2;
+const byte pinButton = 4;
+
+
+
+
+
+
+//States
+volatile byte stateA = HIGH;
+volatile byte stateB = HIGH;
+byte stateButton = HIGH;
+
+unsigned long buttonTimeOut = millis();
+
+
+
+enum LedSettingState
+{
+  SET_BRIGHTNESS,
+  SET_DIFFERENCE
+};
+
+LedSettingState ledSetState = SET_BRIGHTNESS;
+unsigned long buttonDoublePushTimer = 0;
+
+
+
+
+
+//Rotary encoder StateEngine
+enum EncoderState
+{
+  WAIT_ALL,
+
+  //CLOCKWISE STATES
+  CW_WAIT_B_LOW,
+  CW_WAIT_A_HIGH,
+  CW_WAIT_B_HIGH,
+
+  //COUNTERCLOCKWISE STATES
+  CCW_WAIT_A_LOW,
+  CCW_WAIT_B_HIGH,
+  CCW_WAIT_A_HIGH
+
+};
+
+volatile EncoderState eState = WAIT_ALL;
+
+
+RH_NRF24 nrf24;
+
+
+void setup()
+{
+  Serial.begin(9600);
+
+  while (!Serial)
+    ; // wait for serial port to connect. Needed for Leonardo only
+  if (!nrf24.init())
+    Serial.println("init failed");
+  // Defaults after init are 2.402 GHz (channel 2), 2Mbps, 0dBm
+  if (!nrf24.setChannel(1))
+    Serial.println("setChannel failed");
+  if (!nrf24.setRF(RH_NRF24::DataRate2Mbps, RH_NRF24::TransmitPower0dBm))
+    Serial.println("setRF failed");
+
+
+  pinMode(pinA, INPUT);
+  pinMode(pinB, INPUT);
+  pinMode(pinButton, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(pinA), HandleRotaryA, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(pinB), HandleRotaryB, CHANGE);
+}
+
+
+
+void loop()
+{
+  ReadButton();
+}
+
+
+
+
+void ReadButton()
+{
+  if (millis() - buttonTimeOut > 80)
+  {
+    byte currentButtonState = digitalRead(pinButton);
+    if (stateButton == LOW && currentButtonState == HIGH)
+      ButtonPushed();
+
+    stateButton = currentButtonState;
+
+    buttonTimeOut = millis();
+  }
+}
+
+
+void ButtonPushed()
+{
+  LedSettingState lst = ledSetState;
+  switch (ledSetState)
+  {
+    case SET_BRIGHTNESS:
+      ledSetState = SET_DIFFERENCE;
+      break;
+
+    case SET_DIFFERENCE:
+      ledSetState = SET_BRIGHTNESS;
+      break;
+  }
+
+  if (millis() - buttonDoublePushTimer < 400)
+  {
+    Serial.println("Double button");
+    uint8_t data[2] = {SERVER_ADDRESS, 0xAA};
+    nrf24.send(data, sizeof(data));
+  }
+  else
+    buttonDoublePushTimer = millis();
+
+  Serial.println("BUTTON");
+}
+
+
+
+
+void Rotate(int dir)
+{
+  uint8_t data[2];
+  data[0] = SERVER_ADDRESS;
+
+  eState = WAIT_ALL;
+
+  if (dir == CW)
+  {
+    Serial.println("CW");
+    if (ledSetState == SET_DIFFERENCE)
+      data[1] = 0xBB;
+
+    if (ledSetState == SET_BRIGHTNESS)
+      data[1] = 0xDD;
+  }
+  else if (dir == CCW)
+  {
+    Serial.println("CCW");
+    if (ledSetState == SET_DIFFERENCE)
+      data[1] = 0xCC;
+
+    if (ledSetState == SET_BRIGHTNESS)
+      data[1] = 0xEE;
+  }
+
+  nrf24.send(data, sizeof(data));
+}
+
+
+
+
+void HandleRotary(int pin, byte state)
+{
+  if (pin == pinA)
+  {
+    switch (eState)
+    {
+      case WAIT_ALL:
+        if (state == LOW)
+          eState = CW_WAIT_B_LOW;
+        break;
+
+      case CW_WAIT_B_LOW:
+        if (state == HIGH)
+          eState = WAIT_ALL;
+        break;
+
+      case CW_WAIT_A_HIGH:
+        if (state == HIGH)
+          eState = CW_WAIT_B_HIGH;
+        break;
+
+      case CW_WAIT_B_HIGH:
+        if (state == LOW)
+          eState = CW_WAIT_A_HIGH;
+        break;
+
+      case CCW_WAIT_A_LOW:
+        if (state == LOW)
+          eState = CCW_WAIT_B_HIGH;
+        break;
+
+      case CCW_WAIT_B_HIGH:
+        if (state == HIGH)
+          CCW_WAIT_A_LOW;
+        break;
+
+      case CCW_WAIT_A_HIGH:
+        if (state == HIGH)
+          Rotate(CCW);
+        break;
+    }
+  }
+
+  else if (pin == pinB)
+  {
+    switch (eState)
+    {
+      case WAIT_ALL:
+        if (state == LOW)
+          eState = CCW_WAIT_A_LOW;
+        break;
+
+      case CW_WAIT_B_LOW:
+        if (state == LOW)
+          eState = CW_WAIT_A_HIGH;
+        break;
+
+      case CW_WAIT_A_HIGH:
+        if (state == HIGH)
+          eState = CW_WAIT_B_LOW;
+        break;
+
+      case CW_WAIT_B_HIGH:
+        if (state == HIGH)
+          Rotate(CW);
+        break;
+
+      case CCW_WAIT_A_LOW:
+        if (state == HIGH)
+          eState = WAIT_ALL;
+        break;
+
+      case CCW_WAIT_B_HIGH:
+        if (state == HIGH)
+          eState = CCW_WAIT_A_HIGH;
+        break;
+
+      case CCW_WAIT_A_HIGH:
+        if (state == LOW)
+          eState = CCW_WAIT_B_HIGH;
+        break;
+    }
+
+  }
+
+  if (stateA == HIGH && stateB == HIGH)
+    eState = WAIT_ALL;
+}
+
+
+
+
+void HandleRotaryA()
+{
+  byte newState = digitalRead(pinA);
+  if (stateA != newState)
+  {
+    stateA = newState;
+    HandleRotary(pinA, stateA);
+  }
+}
+
+
+
+
+void HandleRotaryB()
+{
+  byte newState = digitalRead(pinB);
+  if (stateB != newState)
+  {
+    stateB = newState;
+    HandleRotary(pinB, stateB);
+  }
+}
+
+
